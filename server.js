83995e3837d24ea0f2aeda63a367a9658bb89bf6
Aaron Becker <aaron.becker@gmail.com>
2021-05-20 01:22:37 -0700

    proper client count tracking

--- server.js
+++ server.js
@@ -29,22 +29,21 @@ TODOS
 proper client close
  */
 
+/* Dependency initialization */
 
- /* Dependency initialization */
-
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const ejs = require('ejs');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const ejs = require("ejs");
 const expressApp = express();
-const http = require('http').Server(expressApp);
-const io = require('socket.io')(http, {
+const http = require("http").Server(expressApp);
+const io = require("socket.io")(http, {
   cors: {
     origin: "http://localhost",
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
   },
-  'pingTimeout': 180000,
-  'pingInterval': 25000
+  pingTimeout: 180000,
+  pingInterval: 25000,
 });
 
 //Addtl core deps
@@ -53,77 +52,85 @@ const RequestHandler = require("./core/requestHandler.js");
 let currentState = "reset";
 let clientCount = 0;
 let userChoices = {};
-io.on('connection', client => {
-	clientCount++;
-	io.emit("console-client-count", clientCount);
-	if (currentState == "reset") {
-		io.emit("demo-change", "reset");
-	} else {
-		io.emit("demo-change", "wait");
-	}
+io.on("connection", (client) => {
+  clientCount++;
+  io.emit("console-client-count", clientCount);
+  if (currentState == "reset") {
+    io.emit("demo-change", "reset");
+  } else {
+    io.emit("demo-change", "wait");
+  }
 
-	client.on("console-event", event => {
-		if (typeof event != "undefined" && typeof event.name != "undefined") {
-			console.log("got console event: ", event.name);
+  client.on("console-event", (event) => {
+    if (typeof event != "undefined" && typeof event.name != "undefined") {
+      console.log("got console event: ", event.name);
 
-			switch (event.name) {
-				case "reset-responses":
-					let uKeys = Object.keys(userChoices);
-					for (let i=0; i<uKeys.length; i++) {
-						userChoices[uKeys[i]] = [];
-					}
-					io.emit("console-event-done", "Reset responses OK");
-					break;
-				default:
-					currentState = event.name;
-					userChoices[currentState] = [];
-					io.emit("demo-change", currentState);
+      switch (event.name) {
+        case "reset-responses":
+          let uKeys = Object.keys(userChoices);
+          for (let i = 0; i < uKeys.length; i++) {
+            userChoices[uKeys[i]] = [];
+          }
+          io.emit("console-event-done", "Reset responses OK");
+          break;
+        default:
+          currentState = event.name;
+          userChoices[currentState] = [];
+          io.emit("demo-change", currentState);
 
-					io.emit("console-event-done", "Successfully processed event '"+event.name+"'");
-					break;
-			}
-		}
-	})
+          io.emit(
+            "console-event-done",
+            "Successfully processed event '" + event.name + "'"
+          );
+          break;
+      }
+    }
+  });
 
-	client.on("demo-choice", choice => {
-		userChoices[currentState].push(choice);
-	})
+  client.on("demo-choice", (choice) => {
+    userChoices[currentState].push(choice);
+  });
+
+  client.on("disconnect", () => {
+    clientCount--;
+    io.emit("console-client-count", clientCount);
+  });
 });
 
 setInterval(() => {
-	io.emit("console-choices", userChoices);
+  io.emit("console-choices", userChoices);
 }, 500);
 
-io.on('close', () => {
-	console.log("Client has disconnected");
-	clientCount--;
-	io.emit("console-client-count", clientCount);
-})
+io.on("close", () => {
+  console.log("Client has disconnected");
+  clientCount--;
+  io.emit("console-client-count", clientCount);
+});
 
+expressApp.use(express.static(path.join(__dirname, "assets"))); //define a static assets directory
+expressApp.set("view engine", "ejs"); //ejs gang
+expressApp.set("views", path.join(__dirname, "views"));
 
-expressApp.use(express.static(path.join(__dirname,"assets"))); //define a static assets directory
-expressApp.set('view engine', 'ejs'); //ejs gang
-expressApp.set('views', path.join(__dirname, 'views'))
-
-expressApp.get('/status', (req, res) => {
-    return res.end(RequestHandler.SUCCESS());
+expressApp.get("/status", (req, res) => {
+  return res.end(RequestHandler.SUCCESS());
 });
 
 expressApp.get("/console", (req, res) => {
-	res.render('console');
+  res.render("console");
 });
 
 expressApp.get("/client", (req, res) => {
-	res.render('client');
-})
+  res.render("client");
+});
 
-expressApp.use(function(req, res, next){ //anything else that doesn't match those filters
-	res.redirect('client');
+expressApp.use(function (req, res, next) {
+  //anything else that doesn't match those filters
+  res.redirect("client");
 });
 
 console.log("Starting server");
 const port = process.env.PORT || 80;
 
 const server = http.listen(port, () => {
-  console.log('LitTester is running on port', server.address().port);
+  console.log("LitTester is running on port", server.address().port);
 });
